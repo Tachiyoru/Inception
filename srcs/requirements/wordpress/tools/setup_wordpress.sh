bf22630a8d9b8229d02d14aa4380334baf5b3d09
Shanley LEON <sleon@e1r10p1.clusters.42paris.fr>
2023-06-20 14:11:18 +0200

    [fix volumes/bash]

--- srcs/requirements/wordpress/tools/setup_wordpress.sh
+++ srcs/requirements/wordpress/tools/setup_wordpress.sh
@@ -12,8 +12,6 @@ else
 	wp config create --allow-root --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=MARIADB:3306 --path=${WP_WEBSITE_PATH}
 	wp core install --allow-root --url=${WP_DOMAIN_NAME} --title=${WP_WEBSITE_NAME} --admin_user=${WP_ADMIN} --admin_password=${WP_ADMIN_PASS} --admin_email=${WP_ADMIN_MAIL}
 	wp user create --allow-root ${WP_USER} ${WP_USER_MAIL} --role=author --user_pass=${WP_USER_PASS}
-	# wp theme install twentyseventeen --activate --allow-root
-	#wp them update twentyseventeen --allow-root
 
 	wp cache flush --allow-root
 fi
