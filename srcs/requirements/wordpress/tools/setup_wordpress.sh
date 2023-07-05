3fd522f90fbdbcd1bc0aa0967c62e828ee1346e6
user <shanley@homail.fr>
2023-07-05 15:07:08 -0400

    test

--- srcs/requirements/wordpress/tools/setup_wordpress.sh
+++ srcs/requirements/wordpress/tools/setup_wordpress.sh
@@ -6,14 +6,17 @@ then
 else
 	echo "DOWNLOADING WP"
 	wp core download --allow-root --locale=fr_FR
+	echo "test1"
+	chmod 777 /run/php
+	echo "test1-2"
 	wp config create --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=MARIADB:3306 --path=$WP_WEBSITE_PATH
+	echo "test2"
 	wp core install --allow-root --url=$WP_DOMAIN_NAME --title=$WP_WEBSITE_NAME --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_MAIL
+	echo "test3"
 	wp user create --allow-root $WP_USER $WP_USER_MAIL --role=author --user_pass=$WP_USER_PASS
 
 fi
 
-#mkdir /run/php
-#chmod 777 /run/php
 
 exec /usr/sbin/php-fpm7.3 -F -R
 
