a14d8f472d77a6833759c18ff23611a288936dd6
user <shanley@homail.fr>
2023-07-06 13:35:36 -0400

    tentative

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 service mysql start
 
-echo "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE} ;" > db1.sql
-echo "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}' ;" >> db1.sql
-echo "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'%' ;" >> db1.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY ${SQL_ROOT_PASSWORD} ;" >> db1.sql
+echo "CREATE DATABASE IF NOT EXISTS inception ;" > db1.sql
+echo "FLUSH PRIVILEGES;" >> db1.sql
+echo "GRANT ALL ON *.* TO '${MYSQL_ROOT_USER}'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' WITH GRANT OPTION;" >> db1.sql
+echo "FLUSH PRIVILEGES;" >> db1.sql
+echo "CREATE USER IF NOT EXIST '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';" >> db1.sql
+echo "GRANT ALL ON inception.* TO '$MYSQL_USER'@'%' WITH GRANT OPTION;"  >> db1.sql
 echo "FLUSH PRIVILEGES;" >> db1.sql
 mysql < db1.sql
 
 kill $(cat /var/run/mysqld/mysqld.pid)
 echo "AAAAAAAAAAAAAAAAAAAAAAAAAA"
 
-mysqld
+exec mysqld
