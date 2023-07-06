6b54c8fcc9debe35184ca0c610441a43c7d15b7b
Shanley LEON <sleon@e1r10p1.clusters.42paris.fr>
2023-07-06 14:52:11 +0200

    test

--- srcs/requirements/wordpress/tools/setup_wordpress.sh
+++ srcs/requirements/wordpress/tools/setup_wordpress.sh
@@ -1,27 +1,14 @@
 #!/bin/bash
 
-echo BEGIN
-cp /etc/wp-config.php .
-wp core download --allow-root
-
-echo PASS
-
-until mysqladmin -hmysql -u$MYSQL_USER -p$MYSQL_PASSWORD ping;
-do
-	echo WAITING
-	sleep 1
-done
-echo CONNECT
-
-#                --- https://developer.wordpress.org/cli/commands/user/create/ ---
-wp core install --url="rleseur.42.fr" --title="Inception"\
-	--admin_user=$WP_ADMIN_LOGIN --admin_password=$WP_ADMIN_PASSWORD --admin_email="test@test.test" --allow-root
-
-if [[ ! $(wp user get $WP_USER_LOGIN --allow-root) ]]
+if [ -f ./wp-config.php ]
 then
-	wp user create $WP_USER_LOGIN inception@example.com --user_pass=$WP_USER_PASSWORD --allow-root
+    echo "Wordpress already installed, skipping download and configuration."
+else
+    wp core download --allow-root --force \
+    && wp config create --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=$SQL_HOSTNAME --allow-root \
+    && wp core install --url=https://localhost --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_MAIL --allow-root \
+    && wp user create $WP_USER $WP_USER_MAIL --role=author --user_pass=$WP_USER_PASSWORD --porcelain --allow-root
 fi
 
-echo GOOD
-
-php-fpm7.3 -F -R
+set -x
+echo $PWD
