71c8d803ea2f1c392d26c907da74dac383db480f
user <shanley@homail.fr>
2023-07-07 11:53:32 -0400

    errmsg

--- srcs/requirements/wordpress/tools/setup_wordpress.sh
+++ srcs/requirements/wordpress/tools/setup_wordpress.sh
@@ -5,15 +5,16 @@ if [ -f ./wp-config.php ]
 then
     echo "Wordpress already installed, skipping download and configuration."
 else
-    wp core download --allow-root
+    wp core download --allow-root --force
 
     echo "HERE1"
-    wp config create --dbname=wordpress --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb --allow-root
+    wp config create --dbname=wordpress --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb --allow-root --force
     echo "HERE2"
     wp core install --url="sleon.42.fr" --title="Inception" \
-    --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_MAIL --allow-root
+    --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_MAIL --allow-root --force
 
-    wp user create $WP_USER $WP_USER_MAIL --user_pass=$WP_USER_PASSWORD --allow-root
+    wp user create $WP_USER $WP_USER_MAIL --user_pass=$WP_USER_PASSWORD --allow-root --force
+    wp config shuffled-salts --allow-root --force
 fi
 
 set -x
