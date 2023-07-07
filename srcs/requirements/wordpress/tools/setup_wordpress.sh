a462e727bce0c7454f9f960f53f6bdf5f1ad22a3
user <shanley@homail.fr>
2023-07-07 11:45:04 -0400

    fix wordpress (partie 452)

--- srcs/requirements/wordpress/tools/setup_wordpress.sh
+++ srcs/requirements/wordpress/tools/setup_wordpress.sh
@@ -5,10 +5,11 @@ if [ -f ./wp-config.php ]
 then
     echo "Wordpress already installed, skipping download and configuration."
 else
-    wp core download --allow-root --force
+    wp core download --allow-root
 
     echo "HERE1"
-    wp config create --dbname=wordpress --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --allow-root
+    wp config create --dbname=wordpress --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb --allow-root
+    echo "HERE2"
     wp core install --url="sleon.42.fr" --title="Inception" \
     --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_MAIL --allow-root
 
